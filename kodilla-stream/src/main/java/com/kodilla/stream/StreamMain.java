e066d9d755c66e74056aff260a3736ead8c55523
Djaxu <koparr@gmail.com>
2018-01-08 16:00:29 +0100

    lekko poprawione bez dobrej daty

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,19 +2,20 @@ package com.kodilla.stream;
 
 
 
-import com.kodilla.stream.forumuser.Forum;
-import com.kodilla.stream.forumuser.ForumUser;
+        import com.kodilla.stream.forumuser.Forum;
+        import com.kodilla.stream.forumuser.ForumUser;
 
 
-import java.time.LocalDate;
-import java.util.Map;
-import java.util.stream.Collectors;
+        import java.time.LocalDate;
+        import java.util.Map;
+        import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
         Forum theForum = new Forum();
         Map<Integer, ForumUser> theResultMapOfForumUsers = theForum.getUserList().stream()
-                .filter(user -> user.getBirthDate().isBefore(LocalDate.ofYearDay(1999,59)) )
+                .filter(user -> user.getBirthDate().isBefore(LocalDate.of(1997,1,1)))
+                .filter(user -> user.getUserSex()>='M' )
                 .filter(user -> user.getNuberOfPosts()>0)
                 .collect(Collectors.toMap(ForumUser::getUserId, user -> user));
 
--- kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
@@ -9,11 +9,11 @@ public class Forum {
     private final List<ForumUser> theForumList = new ArrayList<>();
 
     public Forum(){
-        theForumList.add(new ForumUser(1,"User1",'M',LocalDate.ofYearDay(1989,8),5));
-        theForumList.add(new ForumUser(2,"User2",'F',LocalDate.ofYearDay(1999,28),3));
-        theForumList.add(new ForumUser(3,"User3",'M',LocalDate.ofYearDay(1979,13),4));
-        theForumList.add(new ForumUser(4,"User4",'F',LocalDate.ofYearDay(1999,2),0));
-        theForumList.add(new ForumUser(5,"User5",'M',LocalDate.ofYearDay(2005,1),5));
+        theForumList.add(new ForumUser(1,"User1",'M',LocalDate.of(1989,8, 1),5));
+        theForumList.add(new ForumUser(2,"User2",'F',LocalDate.of(1949,8, 1),3));
+        theForumList.add(new ForumUser(3,"User3",'M',LocalDate.of(1996,8, 1),4));
+        theForumList.add(new ForumUser(4,"User4",'F',LocalDate.of(2000,8, 1),0));
+        theForumList.add(new ForumUser(5,"User5",'M',LocalDate.of(1999,8, 1),5));
     }
     public List<ForumUser> getUserList(){
         return new ArrayList<>(theForumList);

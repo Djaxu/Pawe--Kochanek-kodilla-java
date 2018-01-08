62f2f29079cb124da6c967434709893c1733935f
Djaxu <koparr@gmail.com>
2018-01-08 17:28:22 +0100

    7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -7,15 +7,20 @@ package com.kodilla.stream;
 
 
         import java.time.LocalDate;
+        import java.time.Month;
+        import java.time.Period;
+        import java.time.temporal.ChronoUnit;
         import java.util.Map;
         import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
+
+
         Forum theForum = new Forum();
         Map<Integer, ForumUser> theResultMapOfForumUsers = theForum.getUserList().stream()
-                .filter(user -> user.getBirthDate().isBefore(LocalDate.of(1997,1,1)))
-                .filter(user -> user.getUserSex()>='M' )
+                .filter(user -> ChronoUnit.YEARS.between(user.getBirthDate(),LocalDate.now())>20)
+                .filter(user -> user.getUserSex()=='M' )
                 .filter(user -> user.getNuberOfPosts()>0)
                 .collect(Collectors.toMap(ForumUser::getUserId, user -> user));
 
